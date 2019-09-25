b5390a55209fc764f5c079a669982c4cae15b091
Jon Ruskin <jon.ruskin@gmail.com>
2019-09-25 12:06:38 -0700

    more comments

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const path = require('path');
 const stream = require('stream');
 
 async function runAction(actionScript, options) {
+  // always run the loader first
   const nodeArgs = [
     path.join(__dirname, 'lib', 'loader')
   ];
 
   const execOptions = { ...options };
   execOptions.env = {
-    ...process.env,
-    ...execOptions.env
+    ...process.env, // send process.env to action under test
+    ...execOptions.env // any passed in env overwrites process.env
   };
 
   if (options.mocks) {
+    // try to find a mock file for each requested mocks property
+    // keys should match actions toolkit package names, i.e. `exec` or `github`
     for(let key in options.mocks) {
       const mockPath = path.join(__dirname, 'lib', 'mocks', `${key}.js`);
       if (await fs.access(mockPath).then(() => true).catch(() => false)) {
+        // load the mock
         nodeArgs.push(mockPath);
+        // send the mock options to the child process through ENV
         execOptions.env[`${key.toUpperCase()}_MOCKS`] = JSON.stringify(options.mocks[key]);
       }
     }
